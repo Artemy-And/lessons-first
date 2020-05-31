6c043efe1d01ac12ee6499661e9d89f897141e23
Artemy-And <andruschak.artemy@gmail.com>
2020-05-31 13:42:43 +0300

    create new file Hi and style for it

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Hi from "./Hi";
+
+function App() {
+  return (
+    <div>
+      <Hi name="Artemy" surname="Andruschak"/>
+      <Hi name="Artemy" surname="Andruschak"/>
+      <Hi name="Artemy" surname="Andruschak"/>
+      <Hi name="Artemy" surname="Andruschak"/>
+      <Hi name="Artemy" surname="Andruschak"/>
+      <Hi name="Artemy" surname="Andruschak"/>
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
--- Hi.tsx
+++ Hi.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import classes from "./Hi.module.css";
 
-type NameType={
-    name:string
-    surname:string
+
+type NameType = {
+    name: string
+    surname: string
 }
 
-function Hi(props:NameType) {
+function Hi(props: NameType) {
     return <div className={classes.nameStyle}>
         <h2>{props.name} {props.surname}</h2>
 
